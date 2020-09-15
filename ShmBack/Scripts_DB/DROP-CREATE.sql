b0520aa77a1471404c8e15722d979bb8aebc0c80
Andre <alcorsi@gmail.com>
2020-09-15 12:55:11 -0300

    nova base de dados

--- ShmBack/Scripts_DB/DROP-CREATE.sql
+++ ShmBack/Scripts_DB/DROP-CREATE.sql
@@ -1,72 +1,30 @@
 USE shm_dev;
 
-DROP TABLE `SHM_Atendimentos`;
-DROP TABLE `SHM_Cliente`;
-DROP TABLE `SHM_ClientePonto`;
-DROP TABLE `SHM_ClientePrestador`;
-DROP TABLE `SHM_ClienteTelefone`;
-DROP TABLE `SHM_Config`;
-DROP TABLE `SHM_Especializacao`;
-DROP TABLE `SHM_PontoAtend`;
-DROP TABLE `SHM_PontoAtendTelefone`;
-DROP TABLE `SHM_PontoAtendTipo`;
-DROP TABLE `SHM_PontoPrestador`;
-DROP TABLE `SHM_Prestadores`;
-DROP TABLE `SHM_PrestadoresEsp`;
-DROP TABLE `SHM_PrestadoresTelefones`;
-DROP TABLE `SHM_Relatorios`;
-DROP TABLE `SHM_UsuarioGrupo`;
-DROP TABLE `SHM_Usuarios`;
-DROP TABLE `SHM_UsuariosTelefone`;
+DROP TABLE IF EXISTS `SHM_RelatorioAtendimentos`;
+DROP TABLE IF EXISTS `SHM_Relatorios`;
+DROP TABLE IF EXISTS `SHM_Atendimentos`;
 
+DROP TABLE IF EXISTS `SHM_UsuariosTelefone`;
+DROP TABLE IF EXISTS `SHM_PrestadoresTelefone`;
+DROP TABLE IF EXISTS `SHM_PontosAtendPrestadores`;
+DROP TABLE IF EXISTS `SHM_PrestadoresEsp`;
+DROP TABLE IF EXISTS `SHM_PontosAtendEsp`;
+DROP TABLE IF EXISTS `SHM_PontosAtendTelefone`;
+DROP TABLE IF EXISTS `SHM_ClientesTelefone`;
+DROP TABLE IF EXISTS `SHM_ClientesPrestadores`;
+DROP TABLE IF EXISTS `SHM_ClientesPontosAtend`;
 
-CREATE TABLE `SHM_Atendimentos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `local` int(11) NOT NULL,
-  `entrada` datetime NOT NULL,
-  `saida` datetime DEFAULT NULL,
-  `atendimentos` int(10) unsigned DEFAULT NULL,
-  `valor` int(11) DEFAULT NULL,
-  `medico` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `SHM_Especializacoes`;
+DROP TABLE IF EXISTS `SHM_PontosAtend`;
+DROP TABLE IF EXISTS `SHM_Usuarios`;
+DROP TABLE IF EXISTS `SHM_Prestadores`;
+DROP TABLE IF EXISTS `SHM_Clientes`;
 
-CREATE TABLE `SHM_Cliente` (
-  `iD` int(11) NOT NULL AUTO_INCREMENT,
-  `nomeFantasia` varchar(50) DEFAULT NULL,
-  `razaoSocial` varchar(50) DEFAULT NULL,
-  `cnpj` varchar(14) NOT NULL,
-  `endereco` varchar(50) DEFAULT NULL,
-  `complemento` varchar(20) DEFAULT NULL,
-  `bairro` varchar(20) NOT NULL,
-  `cidade` varchar(50) NOT NULL,
-  `estado` char(2) NOT NULL,
-  `pontoAtendimento` int(11) DEFAULT NULL,
-  `ativo` boolean DEFAULT FALSE,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `SHM_UsuarioGrupo`;
+DROP TABLE IF EXISTS `SHM_Usuario`;
+DROP TABLE IF EXISTS `SHM_Config`;
 
-CREATE TABLE `SHM_ClientePonto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cliente` int(11) NOT NULL,
-  `ponto` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_ClientePrestador` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cliente` int(11) NOT NULL,
-  `prestador` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_ClienteTelefone` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cliente` int(11) NOT NULL,
-  `telefone` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+-- Configurações
 CREATE TABLE `SHM_Config` (
   `smtpSecure` varchar(200) DEFAULT NULL,
   `hostServidor` varchar(200) DEFAULT NULL,
@@ -81,86 +39,18 @@ CREATE TABLE `SHM_Config` (
   `assuntoEmailRecuperar` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `SHM_Especializacao` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `especializacao` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_PontoAtend` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nomeFantasia` varchar(50) DEFAULT NULL,
-  `razaoSocial` varchar(50) DEFAULT NULL,
-  `cnpj` varchar(14) NOT NULL,
-  `endereco` varchar(50) DEFAULT NULL,
-  `complemento` varchar(20) DEFAULT NULL,
-  `bairro` varchar(20) NOT NULL,
-  `cidade` varchar(50) NOT NULL,
-  `estado` char(2) NOT NULL,
-  `especialidades` int(11) NOT NULL,
-  `tipo` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `SHM_PontoAtendTelefone` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pontoAtendimento` int(11) NOT NULL,
-  `telefone` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_PontoAtendTipo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tipo` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_PontoPrestador` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ponto` int(11) NOT NULL,
-  `prestador` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_Prestadores` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `SHM_Usuario` (
+  `id` int unsigned NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `crm` varchar(10) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `endereco` varchar(50) DEFAULT NULL,
-  `complemento` varchar(20) DEFAULT NULL,
-  `bairro` varchar(20) NOT NULL,
-  `cidade` varchar(50) NOT NULL,
-  `estado` char(2) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_PrestadoresEsp` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `prestador` int(10) NOT NULL,
-  `especializacao` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_PrestadoresTelefones` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `prestador` int(10) NOT NULL,
-  `telefone` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
-CREATE TABLE `SHM_Relatorios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `medico` int(11) NOT NULL,
-  `especialidade` int(11) DEFAULT NULL,
-  `entrada` datetime NOT NULL,
-  `saida` datetime NOT NULL,
-  `atendimentos` int(10) unsigned DEFAULT NULL,
-  `valor` int(11) DEFAULT NULL,
-  `cliente` int(11) DEFAULT NULL,
-  `localAtendimento` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `senha` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `grupo` int NOT NULL,
+  `conta` int NOT NULL,
+  `recuperar` varchar(50) DEFAULT NULL,
+  `fl_excluido` int NOT NULL,
+  `fl_bloqueado` int NOT NULL,
+  `email64` varchar(300) DEFAULT NULL,
+  `token64` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `SHM_UsuarioGrupo` (
@@ -174,10 +64,39 @@ CREATE TABLE `SHM_UsuarioGrupo` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Entidades Fortes
+CREATE TABLE `SHM_Clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ativo` boolean DEFAULT FALSE,
+  `cnpj` varchar(14) NOT NULL,
+  `nomeFantasia` varchar(50) DEFAULT NULL,
+  `razaoSocial` varchar(50) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `complemento` varchar(20) DEFAULT NULL,
+  `bairro` varchar(20) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` char(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_Prestadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpf` varchar(14) NOT NULL,
+  `crm` varchar(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `complemento` varchar(20) DEFAULT NULL,
+  `bairro` varchar(20) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` char(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `SHM_Usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
   `cpf` varchar(14) NOT NULL,
+  `nome` varchar(50) NOT NULL,
   `endereco` varchar(50) DEFAULT NULL,
   `complemento` varchar(20) DEFAULT NULL,
   `bairro` varchar(30) NOT NULL,
@@ -187,9 +106,115 @@ CREATE TABLE `SHM_Usuarios` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `SHM_UsuariosTelefone` (
+CREATE TABLE `SHM_PontosAtend` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario` int(11) NOT NULL,
-  `telefone` varchar(20) DEFAULT NULL,
+  `cnpj` varchar(14) NOT NULL,
+  `nomeFantasia` varchar(50) DEFAULT NULL,
+  `razaoSocial` varchar(50) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `complemento` varchar(20) DEFAULT NULL,
+  `bairro` varchar(20) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` char(2) NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `SHM_Especializacoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `especializacao` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Entidades Fracas
+CREATE TABLE `SHM_ClientesPontosAtend` (
+  `id_cliente` int(11) NOT NULL,
+  `id_ponto` int(11) NOT NULL,
+  FOREIGN KEY (`id_cliente`) REFERENCES shm_dev.SHM_Clientes(`id`),
+  FOREIGN KEY (`id_ponto`) REFERENCES shm_dev.SHM_PontosAtend(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_ClientesPrestadores` (
+  `id_cliente` int(11) NOT NULL,
+  `id_prestador` int(11) NOT NULL,
+  FOREIGN KEY (`id_cliente`) REFERENCES shm_dev.SHM_Clientes(`id`),
+  FOREIGN KEY (`id_prestador`) REFERENCES shm_dev.SHM_Prestadores(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_ClientesTelefone` (
+  `id_cliente` int(11) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  FOREIGN KEY (`id_cliente`) REFERENCES shm_dev.SHM_Clientes(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_PontosAtendEsp` (
+  `id_ponto` int(11) NOT NULL,
+  `id_esp` int(11) NOT NULL,
+  FOREIGN KEY (`id_ponto`) REFERENCES shm_dev.SHM_PontosAtend(`id`),
+  FOREIGN KEY (`id_esp`) REFERENCES shm_dev.SHM_Especializacoes(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_PontosAtendTelefone` (
+  `id_ponto` int(11) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  FOREIGN KEY (`id_ponto`) REFERENCES shm_dev.SHM_PontosAtend(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_PontosAtendPrestadores` (
+  `id_ponto` int(11) NOT NULL,
+  `id_prestador` int(11) NOT NULL,
+  FOREIGN KEY (`id_ponto`) REFERENCES shm_dev.SHM_PontosAtend(`id`),
+  FOREIGN KEY (`id_prestador`) REFERENCES shm_dev.SHM_Prestadores(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_PrestadoresEsp` (
+  `id_prestador` int(11) NOT NULL,
+  `id_esp` int(11) NOT NULL,
+  FOREIGN KEY (`id_prestador`) REFERENCES shm_dev.SHM_Prestadores(`id`),
+  FOREIGN KEY (`id_esp`) REFERENCES shm_dev.SHM_Especializacoes(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_PrestadoresTelefone` (
+  `id_prestador` int(11) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  FOREIGN KEY (`id_prestador`) REFERENCES shm_dev.SHM_Prestadores(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_UsuariosTelefone` (
+  `id_usuario` int(11) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  FOREIGN KEY (`id_usuario`) REFERENCES shm_dev.SHM_Usuarios(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_Atendimentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_prestador` int(11) NOT NULL,
+  `id_ponto` int(11) NOT NULL,
+  `valor` int(11) DEFAULT NULL,
+  `entrada` datetime NOT NULL,
+  `saida` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_prestador`) REFERENCES shm_dev.SHM_Prestadores(`id`),
+  FOREIGN KEY (`id_ponto`) REFERENCES shm_dev.SHM_PontosAtend(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `SHM_Relatorios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_prestador` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `id_ponto` int(11) DEFAULT NULL,
+  `entrada` datetime NOT NULL,
+  `saida` datetime NOT NULL,
+  `valor` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_prestador`) REFERENCES shm_dev.SHM_Prestadores(`id`),
+  FOREIGN KEY (`id_cliente`) REFERENCES shm_dev.SHM_Clientes(`id`),
+  FOREIGN KEY (`id_ponto`) REFERENCES shm_dev.SHM_PontosAtend(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Entidades Fracas Nível 2
+CREATE TABLE `SHM_RelatorioAtendimentos` (
+  `id_relatorio` int(11) NOT NULL,
+  `id_atendimento` int(11) UNIQUE NOT NULL,
+  FOREIGN KEY (`id_relatorio`) REFERENCES shm_dev.SHM_Relatorios(`id`),
+  FOREIGN KEY (`id_atendimento`) REFERENCES shm_dev.SHM_Atendimentos(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
