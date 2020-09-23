44757e1f4cecc5b13615797d86cc11911e8a8f97
Fábio <fabio.duqueestrada@gmail.com>
2020-09-23 09:24:01 -0300

    model - ponto/prest/usuario

---
+++ ShmBack/Model/pontoAtendimento.php
@@ -0,0 +1,122 @@
+<?php
+
+class PontoAtendimento implements JsonSerializable{
+    private $id;
+    private $ativo;
+    private $cnpj;
+    private $nomeFantasia;
+    private $razaoSocial;
+    private $endereco;
+    private $complemento;
+    private $bairro;
+    private $cidade;
+    private $estado;
+    private $cep;
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+    public function getAtivo() {
+		return $this->ativo;
+	}
+
+	public function setAtivo($ativo) {
+		$this->ativo = $ativo;
+    }
+    
+    public function getCnpj() {
+		return $this->cnpj;
+	}
+
+	public function setCnpj($cnpj) {
+		$this->cnpj = $cnpj;
+    }
+
+    public function getNomeFantasia() {
+		return $this->nomeFantasia;
+	}
+
+	public function setNomeFantasia($nomeFantasia) {
+		$this->nomeFantasia = $nomeFantasia;
+    }
+
+    public function getRazaoSocial() {
+		return $this->razaoSocial;
+	}
+
+	public function setRazaoSocial($razaoSocial) {
+		$this->razaoSocial = $razaoSocial;
+    }
+
+    public function getEndereco() {
+		return $this->endereco;
+	}
+
+	public function setEndereco($endereco) {
+		$this->endereco = $endereco;
+    }
+
+    public function getComplemento() {
+		return $this->complemento;
+	}
+
+	public function setComplemento($complemento) {
+		$this->complemento = $complemento;
+    }
+
+    public function getBairro() {
+		return $this->bairro;
+	}
+
+	public function setBairro($bairro) {
+		$this->bairro = $bairro;
+    }
+    
+    public function getCidade() {
+		return $this->cidade;
+	}
+
+	public function setCidade($cidade) {
+		$this->cidade = $cidade;
+    }
+
+    public function getEstado() {
+		return $this->estado;
+	}
+
+	public function setEstado($estado) {
+		$this->estado = $estado;
+    }
+    
+    public function getCep() {
+		return $this->cep;
+	}
+
+	public function setCep($cep) {
+		$this->cep = $cep;
+    }
+
+    public function jsonSerialize(){
+      return 
+        [
+            'id'   => $this->getId(),
+            'ativo' => $this->getAtivo(),
+            'cnpj'   => $this->getCnpj(),
+            'nomeFantasia'   => $this->getNomeFantasia(),
+            'razaoSocial'   => $this->getRazaoSocial(),
+            'endereco'   => $this->getEndereco(),
+            'complemento'   => $this->getComplemento(),
+            'bairro'   => $this->getBairro(),
+            'cidade'   => $this->getCidade(),
+            'estado'   => $this->getEstado(),
+            'cep'   => $this->getCep()
+        ];
+    }
+}
+
+?>
---
+++ ShmBack/Model/prestador.php
@@ -0,0 +1,132 @@
+<?php
+
+class Prestador implements JsonSerializable{
+    private $id;
+    private $ativo;
+    private $cpf;
+    private $crm;
+    private $nome;
+    private $email;
+    private $endereco;
+    private $complemento;
+    private $bairro;
+    private $cidade;
+    private $estado;
+    private $cep;
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+    public function getAtivo() {
+		return $this->ativo;
+	}
+
+	public function setAtivo($ativo) {
+		$this->ativo = $ativo;
+    }
+    
+    public function getCpf() {
+		return $this->cpf;
+	}
+
+	public function setCpf($cpf) {
+		$this->cnpj = $cpf;
+    }
+
+    public function getCrm() {
+		return $this->crm;
+	}
+
+	public function setCrm($crm) {
+		$this->crm = $crm;
+    }
+
+    public function getNome() {
+		return $this->nome;
+	}
+
+	public function SetNome($nome) {
+		$this->nome = $nome;
+    }
+
+    public function getEmail() {
+		return $this->email;
+	}
+
+	public function setEmail($email) {
+		$this->email = $email;
+    }
+
+    public function getEndereco() {
+		return $this->endereco;
+	}
+
+	public function setEndereco($endereco) {
+		$this->endereco = $endereco;
+    }
+
+    public function getComplemento() {
+		return $this->complemento;
+	}
+
+	public function setComplemento($complemento) {
+		$this->complemento = $complemento;
+    }
+
+    public function getBairro() {
+		return $this->bairro;
+	}
+
+	public function setBairro($bairro) {
+		$this->bairro = $bairro;
+    }
+    
+    public function getCidade() {
+		return $this->cidade;
+	}
+
+	public function setCidade($cidade) {
+		$this->cidade = $cidade;
+    }
+
+    public function getEstado() {
+		return $this->estado;
+	}
+
+	public function setEstado($estado) {
+		$this->estado = $estado;
+    }
+    
+    public function getCep() {
+		return $this->cep;
+	}
+
+	public function setCep($cep) {
+		$this->cep = $cep;
+    }
+
+    public function jsonSerialize(){
+      return 
+        [
+            'id'   => $this->getId(),
+            'ativo' => $this->getAtivo(),
+            'cpf'   => $this->getCpf(),
+            'crm'   => $this->getCrm(),
+            'nome'   => $this->getNome(),
+            'email'   => $this->getEmail(),
+            'endereco'   => $this->getEndereco(),
+            'complemento'   => $this->getComplemento(),
+            'bairro'   => $this->getBairro(),
+            'cidade'   => $this->getCidade(),
+            'estado'   => $this->getEstado(),
+            'cep'   => $this->getCep()
+        ];
+    }
+}
+
+?>
